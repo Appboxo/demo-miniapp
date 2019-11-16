2b1f7db11fe5a058072997910156ab09742fa98d
chyngyz <arystanuulu@gmail.com>
2019-11-16 22:03:01 +0600

    Build

---
+++ build/precache-manifest.c99c97bb71b5909a5779be14b4f102cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8280429f7660f61b716fbbd956908bdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c59d382f03a69c96ee9f",
+    "url": "/static/css/main.f3207564.chunk.css"
+  },
+  {
+    "revision": "b6cfc477a4270d051d36",
+    "url": "/static/js/2.01e424e4.chunk.js"
+  },
+  {
+    "revision": "c59d382f03a69c96ee9f",
+    "url": "/static/js/main.69963914.chunk.js"
+  },
+  {
+    "revision": "c7bffddfe4c6b00f14f7",
+    "url": "/static/js/runtime-main.46640c3d.js"
+  },
+  {
+    "revision": "69c27ed5bfdc2da258ab91435657c4fb",
+    "url": "/static/media/cross.69c27ed5.svg"
+  },
+  {
+    "revision": "05bd9f1213f52ee83ec15b566b13967e",
+    "url": "/static/media/people.05bd9f12.svg"
+  }
+]);

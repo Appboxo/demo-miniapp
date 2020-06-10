329fb0289f5c3fbb521f1878eb02e17a9506d3bb
chyngyz <arystanuulu@gmail.com>
2020-06-10 17:11:11 +0600

    Add colors from miniapp settings

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import appboxoSdk from '@appboxo/js-sdk'
+
+appboxoSdk.sendPromise('AppBoxoWebAppGetMiniappSettings').then(data => {
+  const settings = {
+    colors: {
+      primary_color: '#DF1445',
+      secondary_color: '#04C613',
+      tertiary_color: '#00C5DA'
+    },
+    hostapp: {
+      name: 'Super app',
+      logo: '',
+    }
+  }
+  document.head.insertAdjacentHTML('beforeend', `<style>
+    :root {
+      --primary-color: ${settings.colors.primary_color};
+      --secondary-color: ${settings.colors.secondary_color};
+      --tertiary-color: ${settings.colors.tertiary_color};
+    }
+  </style>`)
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

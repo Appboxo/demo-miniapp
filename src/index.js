6eae1f53e12d1d06af3d9326c0fa72ce82b90a15
chyngyz <arystanuulu@gmail.com>
2020-06-10 17:51:28 +0600

    upd

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import appboxoSdk from '@appboxo/js-sdk'
 
 appboxoSdk.sendPromise('AppBoxoWebAppGetMiniappSettings').then(data => {
   console.log('MINIAPP_SETTINGS: ', data)
+  document.head.insertAdjacentHTML('beforeend', `<style>
+    :root {
+      --primary-color: ${data.miniapp_settings.colors.primary_color};
+      --secondary-color: ${data.miniapp_settings.colors.secondary_color};
+      --tertiary-color: ${data.miniapp_settings.colors.tertiary_color};
+    }
+  </style>`)
 }).catch(err => console.log(err));
 
 // setTimeout(() => {

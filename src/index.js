0ec358868d7c9a820f7a9ccdd48d25e44a74322e
chyngyz <arystanuulu@gmail.com>
2020-06-10 17:45:29 +0600

    update

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import appboxoSdk from '@appboxo/js-sdk'
 
-appboxoSdk.send('AppBoxoWebAppGetMiniappSettings')
+appboxoSdk.sendPromise('AppBoxoWebAppGetMiniappSettings').then(data => {
+  console.log('MINIAPP_SETTINGS: ', data)
+}).catch(err => console.log(err));
 
-setTimeout(() => {
-  if (window.miniapp_settings) {
-    document.head.insertAdjacentHTML('beforeend', `<style>
-      :root {
-        --primary-color: ${window.miniapp_settings.colors.primary_color};
-        --secondary-color: ${window.miniapp_settings.colors.secondary_color};
-        --tertiary-color: ${window.miniapp_settings.colors.tertiary_color};
-      }
-    </style>`)
-  }
-}, 4000)
+// setTimeout(() => {
+//   if (window.miniapp_settings) {
+//     document.head.insertAdjacentHTML('beforeend', `<style>
+//       :root {
+//         --primary-color: ${window.miniapp_settings.colors.primary_color};
+//         --secondary-color: ${window.miniapp_settings.colors.secondary_color};
+//         --tertiary-color: ${window.miniapp_settings.colors.tertiary_color};
+//       }
+//     </style>`)
+//   }
+// }, 4000)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
